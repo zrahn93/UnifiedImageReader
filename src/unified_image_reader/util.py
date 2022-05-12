d16c367aa78fc61fd3e646e6a26cccf3183ee501
Adin Solomon <adinbsolomon@gmail.com>
2022-05-12 03:50:36 +0000

    listdir for directory reader

--- src/unified_image_reader/util.py
+++ src/unified_image_reader/util.py
@@ -3,7 +3,8 @@
     Utility functions and classes for the Unified Image Reader
 """
 
-from typing import NewType, Tuple, Union
+import os
+from typing import List, NewType, Tuple, Union
 
 RegionDimensions = NewType('RegionDimensions', Tuple[int, int])
 
@@ -13,3 +14,22 @@ RegionIdentifier = NewType(
     'RegionIdentifier', Union[RegionIndex, RegionCoordinates])
 
 FilePath = NewType('FilePath', str)
+
+
+def listdir_recursive(path: FilePath) -> List[FilePath]:
+    """
+    listdir_recursive lists files (not directories) recursively from path
+
+    :param path: the path to the directory whose files should be listed recursively
+    :type path: FilePath
+    :return: a list of filepaths relative to path
+    :rtype: List[FilePath]
+    """
+    files = []
+    walk = os.walk(path)
+    for (directory_pointer, _, file_nodes) in walk:
+        files += [
+            os.path.join(directory_pointer, file_node)
+            for file_node in file_nodes
+        ]
+    return files
